3221771c97dd369ace41a8754ca342f52dd756d8
unknown <942450441@qq.com>
2020-05-24 10:42:51 +0800

    添加内容

---
+++ Algorithm/LinkedListStudy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public static class LinkedListStudy
+    {
+        public static bool IsPalindromeString(LinkedList<string> str) {
+
+            return false;
+        }
+    }
+}
