4feaa269d591504fa51518e175bd0e1145e68284
Morphling <942450441@qq.com>
2021-01-16 20:28:14 +0800

    78,143

--- Algorithm/MicroSoftSubject_Week11.cs
+++ Algorithm/MicroSoftSubject_Week11.cs
@@ -21,7 +21,7 @@ namespace Algorithm
             {
                 return s;
             }
-       
+
             //构建并查集
             UnionFind uf = new UnionFind(s.Length);
             foreach (var pair in pairs) {
@@ -38,15 +38,15 @@ namespace Algorithm
                 if (map.ContainsKey(root))
                 {
 
-                    map[root].Add(charArr[i]);                   
+                    map[root].Add(charArr[i]);
                 }
                 else {
 
                     map[root] = new List<char>() { charArr[i] };
                 }
             }
-            foreach(var kv in map) { 
-            
+            foreach (var kv in map) {
+
                 kv.Value.Sort((x, y) => x < y ? 1 : -1);
             }
 
@@ -117,7 +117,7 @@ namespace Algorithm
             var mod = 0;
             for (int i = 0; i < A.Length; i++) {
 
-                mod = (mod*2) + A[i];
+                mod = (mod * 2) + A[i];
                 mod = mod % 5;
                 if (mod == 0)
                 {
@@ -204,7 +204,155 @@ namespace Algorithm
             return stones.Length - uf.Count;
         }
 
-        private static int MaxLengthDFS(IList<string> arr,int start,int bitMask) {
+        /// <summary>
+        /// 803. 打砖块
+        /// 有一个 m x n 的二元网格，其中 1 表示砖块，0 表示空白。砖块 稳定（不会掉落）的前提是：
+        ///   一块砖直接连接到网格的顶部，或者
+        ///   至少有一块相邻（4 个方向之一）砖块 稳定 不会掉落时
+        /// 给你一个数组 hits ，这是需要依次消除砖块的位置。每当消除 hits[i] = (rowi, coli) 位置上的砖块时，对应位置的砖块（若存在）会消失，然后其他的砖块可能因为这一消除操作而掉落。一旦砖块掉落，它会立即从网格中消失（即，它不会落在其他稳定的砖块上）。
+        /// 返回一个数组 result ，其中 result[i] 表示第 i 次消除操作对应掉落的砖块数目。
+        /// 注意，消除可能指向是没有砖块的空白位置，如果发生这种情况，则没有砖块掉落。
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <param name="hits"></param>
+        /// <returns></returns>
+        public static int[] HitBricks(int[][] grid, int[][] hits)
+        {
+            int h = grid.Length, w = grid[0].Length;
+
+            UnionFind3 uf = new UnionFind3(h * w + 1);
+            int[][] status = new int[h][];
+            for (int i = 0; i < h; i++)
+            {
+                status[i] = new int[grid[0].Length];
+                for (int j = 0; j < w; j++)
+                {
+                    status[i][j] = grid[i][j];
+                }
+            }
+            for (int i = 0; i < hits.Length; i++)
+            {
+                status[hits[i][0]][hits[i][1]] = 0;
+            }
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    if (status[i][j] == 1)
+                    {
+                        if (i == 0)
+                        {
+                            uf.Merge(h * w, i * w + j);
+                        }
+                        if (i > 0 && status[i - 1][j] == 1)
+                        {
+                            uf.Merge(i * w + j, (i - 1) * w + j);
+                        }
+                        if (j > 0 && status[i][j - 1] == 1)
+                        {
+                            uf.Merge(i * w + j, i * w + j - 1);
+                        }
+                    }
+                }
+            }
+            int[][] directions = { new int[]{ 0, 1 }, new int[]{ 1, 0 }, new int[]{ 0, -1 }, new int[]{ -1, 0 } };
+            int[] ret = new int[hits.Length];
+            for (int i = hits.Length - 1; i >= 0; i--)
+            {
+                int r = hits[i][0], c = hits[i][1];
+                if (grid[r][c] == 0)
+                {
+                    continue;
+                }
+                int prev = uf.Size(h * w);
+
+                if (r == 0)
+                {
+                    uf.Merge(c, h * w);
+                }
+                foreach (int[] direction in directions)
+                {
+                    int dr = direction[0], dc = direction[1];
+                    int nr = r + dr, nc = c + dc;
+
+                    if (nr >= 0 && nr < h && nc >= 0 && nc < w)
+                    {
+                        if (status[nr][nc] == 1)
+                        {
+                            uf.Merge(r * w + c, nr * w + nc);
+                        }
+                    }
+                }
+                int size = uf.Size(h * w);
+                ret[i] = Math.Max(0, size - prev - 1);
+                status[r][c] = 1;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 143. 重排链表
+        /// 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+        /// 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+        /// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+        /// </summary>
+        /// <param name="head"></param>
+        public static void ReorderList(ListNode head)
+        {
+            if (head == null || head.next == null) return;
+
+            var nodeStack = new Stack<ListNode>();
+            var current = head;
+            while (current != null) {
+
+                nodeStack.Push(current);
+                current = current.next;
+            }
+            current = head;
+            while (current != null) {
+                var nextNode = current.next;
+                current.next = nodeStack.Pop();
+                if (nextNode == current.next) { 
+                    current.next.next = null;
+                    break;
+                }
+                if (current.next == current) {
+
+                    current.next = null;
+                    break;
+                }
+                current.next.next = nextNode;
+                current = nextNode;
+            }
+
+            //1 -> 2 -> 3 ->4 -> 5
+            //1 -> 2 -> 3 ->4
+
+        }
+
+        public static IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            if (nums == null || nums.Length == 0) return result;
+
+            int n = nums.Length;
+            for (int mask = 0; mask < (1 << n); mask++) {
+                var t = new List<int>();
+                for (int i = 0; i < n; i++) {
+
+                    if ((mask & (1 << i)) != 0) {
+
+                        t.Add(nums[i]);
+                    }
+                }
+
+                result.Add(t);
+            }
+
+            return result;
+        }
+
+        private static int MaxLengthDFS(IList<string> arr, int start, int bitMask) {
 
             if (start == arr.Count) return 0;
 
@@ -214,7 +362,7 @@ namespace Algorithm
                 int bit = GetBitMask(arr[i]);
                 if (bit == 0 || (bit & bitMask) != 0) continue;
 
-                result = Math.Max(result, MaxLengthDFS(arr, i + 1,bit | bitMask) + arr[i].Length);
+                result = Math.Max(result, MaxLengthDFS(arr, i + 1, bit | bitMask) + arr[i].Length);
             }
 
             return result;
@@ -303,7 +451,7 @@ namespace Algorithm
             Dictionary<int, int> parent = new Dictionary<int, int>();
             private int _count = 0;
             public int Count { get { return _count; } }
-            public void Union(int x,int y) {
+            public void Union(int x, int y) {
 
                 var rootX = Find(x);
                 var rootY = Find(y);
@@ -329,6 +477,50 @@ namespace Algorithm
                 return parent[x];
             }
 
+        }
+
+        private class UnionFind3 {
+
+            int[] f;
+            int[] sz;
+
+            public UnionFind3(int n)
+            {
+                f = new int[n];
+                sz = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    f[i] = i;
+                    sz[i] = 1;
+                }
+            }
+
+            public int Find(int x)
+            {
+                if (f[x] == x)
+                {
+                    return x;
+                }
+                int newf = Find(f[x]);
+                f[x] = newf;
+                return f[x];
+            }
+
+            public void Merge(int x, int y)
+            {
+                int fx = Find(x), fy = Find(y);
+                if (fx == fy)
+                {
+                    return;
+                }
+                f[fx] = fy;
+                sz[fy] += sz[fx];
+            }
+
+            public int Size(int x)
+            {
+                return sz[Find(x)];
+            }
         }
     }
 }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,8 +11,16 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            var data = new int[][] { new int[] { 0, 0 }, new int[] { 0, 1 } , new int[] { 1, 0 } , new int[] { 1, 2 } , new int[] {2,1 } , new int[] { 2,2 }};
-            MicroSoftSubject_Week11.RemoveStones(data);
+            var head = new ListNode(1);
+            var node1 = new ListNode(2);
+            head.next = node1;
+            var node2 = new ListNode(3);
+            node1.next = node2;
+            var node3 = new ListNode(4);
+            node2.next = node3;
+            var node4 = new ListNode(5);
+            node3.next = node4;
+            MicroSoftSubject_Week11.ReorderList(head);
         }
 
 
